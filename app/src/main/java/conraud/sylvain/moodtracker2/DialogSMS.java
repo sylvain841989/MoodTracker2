3e079e44a66843218ea7aab93d24fc71e3ff62e3
sylvain841989 <sylvainconraud74@hotmail.fr>
2019-03-07 10:54:12 +0100

    add Send SMS

---
+++ app/src/main/java/conraud/sylvain/moodtracker2/DialogSMS.java
@@ -0,0 +1,89 @@
+package conraud.sylvain.moodtracker2;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class DialogSMS extends DialogFragment {
+
+    String num , message;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        String [] mood = new String[5];
+        mood[0]="Aujourd'hui je suis vraiment triste";
+        mood[1]="Aujourd'hui je ne suis pas au top";
+        mood[2] = "Ca pourrait aller mieux mais ça pourrait être pire";
+        mood[3]="Ajourd'hui ca va plutot bien";
+        mood[4]="Aujourd'hui je suis super content";
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view =inflater.inflate(R.layout.dialog_sms,null);
+        builder.setView(view);
+        builder.setTitle("Send Message");
+        final EditText editTextMsg =view.findViewById(R.id.dialog_sms_edit_text_message);
+        final EditText editTextNum =view.findViewById(R.id.dialog_sms_edit_text_number);
+
+
+        if(Save.mCurrentComment == null){
+            editTextMsg.setText(mood[Save.mCurrentMood]);
+        }else{
+            editTextMsg.setText(mood[Save.mCurrentMood] + " car " +Save.mCurrentComment);
+
+        }
+
+        builder.setPositiveButton("Envoyer", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                num = editTextNum.getText().toString();
+                message = editTextMsg.getText().toString();
+                sendSMS();
+
+            }
+        });
+
+        builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
+        return builder.create();
+    }
+
+    void sendSMS(){
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+            if(num.length() == 10 && message!=null){
+                SmsManager.getDefault().sendTextMessage(num,null,message,null,null);
+                Toast.makeText(getContext(),"SMS envoyé", Toast.LENGTH_SHORT).show();
+
+            }else{
+                Toast.makeText(getContext(),"erreur de saisi",Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.SEND_SMS},1);
+
+        }
+
+
+
+    }
+
+}
--- app/src/main/java/conraud/sylvain/moodtracker2/PageFragment.java
+++ app/src/main/java/conraud/sylvain/moodtracker2/PageFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 
@@ -20,14 +21,9 @@ public class PageFragment extends Fragment {
     private static final String KEY_COLOR = "color";
     public static final String KEY_SMILEY = "smiley";
 
-
-
-
-
     public PageFragment() {
         // Required empty public constructor
     }
-
 
     public static PageFragment newInstance(int position, int color, int smiley) {
         PageFragment fragment = new PageFragment();
@@ -38,9 +34,6 @@ public class PageFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -51,58 +44,77 @@ public class PageFragment extends Fragment {
         ImageView imageViewAddComment = result.findViewById(R.id.fragment_page_image_view_addComment);
         ImageView imageViewHistory = result.findViewById(R.id.fragment_page_image_view_history);
         ImageView imageViewPieChart = result.findViewById(R.id.fragment_page_image_view_pie_chart);
+        ImageView imageViewSendSms = result.findViewById(R.id.fragment_page_image_view_send_sms);
         int color = getArguments().getInt(KEY_COLOR);
         int smiley = getArguments().getInt(KEY_SMILEY);
-
         relativeLayout.setBackgroundColor(color);
         imageViewSmiley.setImageResource(smiley);
 
         imageViewHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), HistoryActivity.class);
-                startActivity(intent);
-            }
-        });
+                openActivityHistory();
+            }});
 
         imageViewAddComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle(R.string.comment);
-                final EditText editText = new EditText(getContext());
-                editText.setText(Save.mCurrentComment);
-                editText.setFilters(new InputFilter[]{ new InputFilter.LengthFilter(150) });
-                builder.setView(editText);
-                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                });
-                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Save.saveComment(editText.getText().toString());
-                    }
-                });
-                builder.create().show();
-
-            }
-        });
+                openDialogComment();
+            }});
 
         imageViewPieChart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(),PieChartActivity.class);
-                startActivity(intent);
+                openActivityPieChart();
+            }});
+
+        imageViewSendSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialogSMS();
             }
         });
-
         return result;
     }
 
+    void openActivityHistory()
+    {
+        Intent intent = new Intent(getContext(), HistoryActivity.class);
+        startActivity(intent);
+    }
 
+    void openDialogComment(){
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.comment);
+        final EditText editText = new EditText(getContext());
+        editText.setText(Save.mCurrentComment);
+        editText.setFilters(new InputFilter[]{ new InputFilter.LengthFilter(150) });
+        builder.setView(editText);
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Save.saveComment(editText.getText().toString());
+            }
+        });
+        builder.create().show();
+    }
+
+    void openActivityPieChart(){
+        Intent intent = new Intent(getContext(),PieChartActivity.class);
+        startActivity(intent);
+    }
+
+    void openDialogSMS(){
+       DialogSMS dialogSMS = new DialogSMS();
+       dialogSMS.show(getActivity().getSupportFragmentManager(),"dialogSMS");
+    }
 
 }
